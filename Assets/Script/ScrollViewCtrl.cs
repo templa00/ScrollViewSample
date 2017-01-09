010fd6a25a1d73f0dbd3d5eaa50a0e272d89a7d1
sk <ggdsdhjj@yahoo.co.jp>
2017-01-10 07:30:44 +0900

    スクロールビュー
    最初のコミット

---
+++ Assets/Script/ScrollViewCtrl.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// スクロールビューコントロール
+/// </summary>
+public class ScrollViewCtrl : MonoBehaviour
+{
+    /// <summary>
+    /// コンテンツの短径の情報
+    /// </summary>
+    [SerializeField]
+    private RectTransform contentRect = null;
+
+    /// <summary>
+    /// ステージリスト
+    /// </summary>
+    [SerializeField]
+    private GameObject stageListObj = null;
+
+    /// <summary>
+    /// ボタンオブジェクト
+    /// </summary>
+    [SerializeField]
+    private GameObject baseBtnObj = null;
+
+    /// <summary>
+    /// 開始
+    /// </summary>
+    void Start ()
+    {
+        // ボタン生成
+        CreateButton(100);
+    }
+
+    /// <summary>
+    /// ボタン生成
+    /// </summary>
+    /// <param name="_num">個数</param>
+    private void CreateButton (int _num)
+    {
+        // ゲームオブジェクトとしてプレファブ(object)を読み込みます。
+        var obj = Instantiate(Resources.Load("Prefabs/Button")) as GameObject;
+
+        for (int i = 0; i < _num; i++)
+        {
+            // objをGameObjectとしてcopy_objに複製する。
+            var copy_obj = UnityEngine.Object.Instantiate(obj) as GameObject;
+
+            copy_obj.transform.parent = stageListObj.transform;
+
+            copy_obj.GetComponent<ButtonCtrl>().text.text = string.Format("ステージ {0}", i + 1);
+        }
+    }
+}
