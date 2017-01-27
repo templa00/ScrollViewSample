ddb57049eac6fe0a485b2f9a4d1d1ad55aa13e4c
templa00 <templa001s@gmail.com>
2017-01-28 00:44:55 +0900

    Revert "不要な変数削除"
    This reverts commit 541d303425001a30fd929a70b2a42198f88f57f7.

--- Assets/Script/ScrollViewCtrl.cs
+++ Assets/Script/ScrollViewCtrl.cs
@@ -8,10 +8,22 @@ using UnityEngine;
 public class ScrollViewCtrl : MonoBehaviour
 {
     /// <summary>
+    /// コンテンツの短径の情報
+    /// </summary>
+    [SerializeField]
+    private RectTransform contentRect = null;
+
+    /// <summary>
     /// ステージリスト
     /// </summary>
     [SerializeField]
     private GameObject stageListObj = null;
+
+    /// <summary>
+    /// ボタンオブジェクト
+    /// </summary>
+    [SerializeField]
+    private GameObject baseBtnObj = null;
 
     /// <summary>
     /// 開始
